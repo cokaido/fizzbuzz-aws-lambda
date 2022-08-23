796c7505e3db74ec1ea0ba24a2290c7862ccbaad
Andres Ramon <andres.ramon@gmail.com>
2022-08-23 16:00:03 +0200

    change application name

--- src/tests/domain/fizzbuzz.test.ts
+++ src/tests/domain/fizzbuzz.test.ts
@@ -2,7 +2,6 @@ import { fizzbuzz_generator } from '../../domain/fizzbuzz';
 
 describe('Test fizzbuzz domain object', () => {
   it('should return Fizz when called with 3', async () => {
-
     const result = fizzbuzz_generator(3);
     const expectedResult = 'Fizz';
 
--- src/tests/handlers/fizzbuzz.test.ts
+++ src/tests/handlers/fizzbuzz.test.ts
@@ -1,10 +1,8 @@
-
 import { handler } from '../../handlers/fizzbuzz';
 import { constructAPIGwEvent } from '../factories/handler-event-factory';
 
 describe('Test fizzbuzz lambda', () => {
   it('should return Fizz when called with 3', async () => {
-
     const event = constructAPIGwEvent({}, { method: 'GET' });
 
     const result = await handler(event);
