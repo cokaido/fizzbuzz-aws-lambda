279939f1cf129de59c8d70cc6239907dc624b388
Pedro Santos <pedro.santos.personal@gmail.com>
2022-10-20 11:28:37 +0200

    improve test messages

--- src/tests/domain/fizzbuzz.test.ts
+++ src/tests/domain/fizzbuzz.test.ts
@@ -6,31 +6,28 @@ describe('FizzBuzz', () => {
     [2, '2'],
     [4, '4'],
     [7, '7'],
-  ])('Fizzbuzz of %s should be "%s"', (input: number, expectedValue: string) => {
+  ])('of %s should be %s', (input: number, expectedValue: string) => {
     expect(fizzBuzz(input)).toBe(expectedValue);
   });
 
-  test.each([
-    [3, 'Fizz'],
-    [6, 'Fizz'],
-    [9, 'Fizz'],
-  ])('multiples of 3 should be "%s"', (input: number, expectedValue: string) => {
-    expect(fizzBuzz(input)).toBe(expectedValue);
-  });
+  test.each([[3], [6], [9]])(
+    'of multiples of 3 (example %s) should be "Fizz"',
+    (input: number, expectedValue = 'Fizz') => {
+      expect(fizzBuzz(input)).toBe(expectedValue);
+    }
+  );
 
-  test.each([
-    [5, 'Buzz'],
-    [10, 'Buzz'],
-    [20, 'Buzz'],
-  ])('multiples of 5 should be "%s"', (input: number, expectedValue: string) => {
-    expect(fizzBuzz(input)).toBe(expectedValue);
-  });
+  test.each([[5], [10], [20]])(
+    'of multiples of 5 (example %s) should be "Buzz"',
+    (input: number, expectedValue = 'Buzz') => {
+      expect(fizzBuzz(input)).toBe(expectedValue);
+    }
+  );
 
-  test.each([
-    [15, 'FizzBuzz'],
-    [30, 'FizzBuzz'],
-    [45, 'FizzBuzz'],
-  ])('multiples of both 3 and 5 should be "%s"', (input: number, expectedValue: string) => {
-    expect(fizzBuzz(input)).toBe(expectedValue);
-  });
+  test.each([[15], [30], [45]])(
+    'of multiples of both 3 and 5 (for example %s) should be "FizzBuzz"',
+    (input: number, expectedValue = 'FizzBuzz') => {
+      expect(fizzBuzz(input)).toBe(expectedValue);
+    }
+  );
 });
